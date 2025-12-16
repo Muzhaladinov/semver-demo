97457a9dec890f999096dfc76a819f63e33c57c9
Muzhaladinov <magomedmuzhal@gmail.com>
2025-12-16 11:37:17 +0300

    Fix null name bug (v1.1.1)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        greetUser("User");
     }
+
+    public static void greetUser(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+public static void greetUser(String name) {
+    if (name == null || name.isBlank()) {
+        name = "Guest";
+    }
+    System.out.println("Hello, " + name + "!");
+} 
 }
